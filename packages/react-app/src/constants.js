a138e8b11e13a3aa3e644cd86f43a247634834ee
rabson-dao <rabson.dao@yandex.ru>
2022-11-27 19:49:51 +0300

    infura update real

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,8 +1,8 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "5b3aa68d82264f59bb6a1874cb3c23ea"; // trying the  emoji.support key
+export const INFURA_ID = "293777dcd6a442f1807547190a943e4c"; // trying the  emoji.support key
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
+export const ETHERSCAN_KEY = "HTTV68J9AVB6V8NRW7JQXB16APMIEUDWP8";
 
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
