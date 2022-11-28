86679994ec39ddc2cd793e220b261a0c76a59a88
rabson-dao <rabson.dao@yandex.ru>
2022-11-28 12:33:18 +0300

    added bank

--- packages/react-app/src/components/GasGauge.jsx
+++ packages/react-app/src/components/GasGauge.jsx
@@ -31,11 +31,11 @@ export default function GasGauge(props) {
     >
       <span style={{ marginRight: 8 }}>
         <span role="img" aria-label="fuelpump">
-          {sber}
+          <img src={sber} />
           {/* ⛽️ */}
         </span>
       </span>
-      {typeof props.gasPrice === "undefined" ? 0 : parseInt(props.gasPrice, 10) / 10 ** 9}g
+      {/* {typeof props.gasPrice === "undefined" ? 0 : parseInt(props.gasPrice, 10) / 10 ** 9}g */}
     </Button>
   );
 }
