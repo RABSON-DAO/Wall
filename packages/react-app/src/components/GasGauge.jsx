0e2013934fb310df5285b3a6395a55d75abc7fe0
rabson-dao <rabson.dao@yandex.ru>
2022-11-28 15:05:04 +0300

    added bank

--- packages/react-app/src/components/GasGauge.jsx
+++ packages/react-app/src/components/GasGauge.jsx
@@ -23,7 +23,7 @@ export default function GasGauge(props) {
   return (
     <Button
       onClick={() => {
-        window.open("https://ethgasstation.info/");
+        window.open("https://wmzsale.biz/exchange-USDTERC20-to-SBERRUB/");
       }}
       size="large"
       shape="round"
