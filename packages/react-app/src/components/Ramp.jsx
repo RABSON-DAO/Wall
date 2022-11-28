9a51fe78f2eb1caf26908335cd0484b21564b829
rabson-dao <rabson.dao@yandex.ru>
2022-11-28 10:32:38 +0300

    infura ramp fix

--- packages/react-app/src/components/Ramp.jsx
+++ packages/react-app/src/components/Ramp.jsx
@@ -108,7 +108,7 @@ export default function Ramp(props) {
                 hostAppName: "scaffold-eth",
                 hostLogoUrl: "https://scaffoldeth.io/scaffold-eth.png",
                 swapAmount: "100",//"100000000000000000", // 0.1 ETH in wei  ?
-                swapAsset: "ETH_USDT",
+                swapAsset: "ETH",
                 userAddress: props.address,
               })
                 .on("*", event => console.log(event))
