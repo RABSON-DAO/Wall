d018ce545ec468ba949539e1e0103c467c4aeedb
rabson-dao <rabson.dao@yandex.ru>
2022-11-28 12:57:19 +0300

    added bank

--- packages/react-app/src/components/GasGauge.jsx
+++ packages/react-app/src/components/GasGauge.jsx
@@ -29,7 +29,7 @@ export default function GasGauge(props) {
       size="large"
       shape="round"
     >
-      <img src="../sber.svg"/>
+      <img src="../sberbank.png"/>
       {/*<span style={{ marginRight: 8 }}>
         <span role="img" aria-label="fuelpump">
           
