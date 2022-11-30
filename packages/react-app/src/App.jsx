ecbc84bf70bbf8d3a3836498826eda07ed5bf4a9
rabson-dao <rabson.dao@yandex.ru>
2022-11-30 20:44:06 +0300

    added Main Info Central Pic

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -910,7 +910,7 @@ function App(props) {
       <div style={{ padding: 16, cursor: "pointer", backgroundColor: "#FFFFFF", width: 420, margin: "auto" }}>      
         <Splash />
       </div>
-      
+      <div>Your Crypto Wallet Balance:</div>
       <div 
         style={{ clear: "both", opacity: yourLocalBalance ? 1 : 0.2, width: 500, margin: "auto", position: "relative" }}
       >
