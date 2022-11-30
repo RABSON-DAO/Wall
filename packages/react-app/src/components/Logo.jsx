05259aed9bf61ad6883bdf00032935491302f948
rabson-dao <rabson.dao@yandex.ru>
2022-11-30 14:29:44 +0300

    added logo

--- packages/react-app/src/components/Logo.jsx
+++ packages/react-app/src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import image from "../logo.svg";
 function Logo() {
-   return <img src={image} alt="Rabson" />;
+   return <img src={image} style="height:1em" alt="Rabson" />;
 }
 export default Logo; 
